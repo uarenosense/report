ca47e9ed64fb03bfdc0a6e77e34fb93ee90d1a23
李伟 <hzliwei@corp.netease.com>
2015-05-21 19:19:33 +0800

    misc:accounts

---
+++ client/src/js/app.js
@@ -0,0 +1,37 @@
+angular.module('app', ['ngRoute'])
+    .controller('accounts', ['$scope', '$http', function($scope, $http){
+        $http.get('/user/list')
+            .success(function(data){
+                $scope.users = data.users;
+            });
+        $scope.delete = function(user){
+            $http.get('/user/delete?id='+user['_id'])
+                .success(function(data){
+                    if(data.code==200){
+                        var index = $scope.users.indexOf(user);
+                        if(index!=-1){
+                            $scope.users.splice(index, 1);
+                        }
+                    }
+                });
+        };
+        $scope.updateRole = function(user){
+            $http.get('/user/update/role?'+jQuery.param({id:user['_id'], role:user.role}))
+                .success(function(data){
+                    if(data.code==200){
+
+                    }else{
+                        if(user.role=='normal'){
+                            user.role='leader';
+                        }else{
+                            user.role='normal';
+                        }
+                    }
+                });
+        };
+    }])
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider
+            .when('/accounts', {templateUrl:'accounts', controller:'accounts'})
+            .otherwise({redirectTo: '/my'});
+    }]);
--- server/config.js
+++ server/config.js
@@ -10,6 +10,6 @@ module.exports = {
         url:'mongodb://localhost:27017/report'
     },
     security:{
-        admins:['liwei']
+        admins:['hzliwei@126.com']
     }
 };
